eb1b1ee77997b189388f65405c3d4fc25d34643f
Robert Christensen <robert.christensen@mymail.champlain.edu>
2022-11-27 10:41:13 -0600

    Added stay-in-area and targeted wander behaviours.

---
+++ Revolver/Assets/Combat/Enemy/AI/ContextMaps/StayInAreaContext.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StayInAreaContext : IContextProvider
+{
+    [SerializeField] private Transform anchor;
+    [SerializeField] [Min(0)] private float neutralRange = 10;
+    
+    protected override void RefreshContextMapValues()
+    {
+        for (int i = 0; i < entries.Length; ++i)
+        {
+            Vector3 toAnchor = anchor.position - ContextMap.Host.transform.position;
+            toAnchor.y = 0;
+            float distToAnchor = toAnchor.magnitude;
+            float angleToAnchor = Vector3.Angle(toAnchor, entries[i].direction) * Mathf.Deg2Rad;
+            entries[i].value += shapingFunction.Evaluate(angleToAnchor) * Mathf.Max(0, distToAnchor-neutralRange)*weight;
+        }
+    }
+}
