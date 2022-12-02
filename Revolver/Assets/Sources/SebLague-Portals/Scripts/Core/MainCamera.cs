673e5bc65856d9922c212e17d184ab77c72a63eb
Robert Christensen <robert.christensen@mymail.champlain.edu>
2022-12-02 17:17:34 -0500

    Added Sebastian Lague's Portals

---
+++ Revolver/Assets/Sources/SebLague-Portals/Scripts/Core/MainCamera.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class MainCamera : MonoBehaviour {
+
+    Portal[] portals;
+
+    void Awake () {
+        portals = FindObjectsOfType<Portal> ();
+
+        RenderPipelineManager.beginCameraRendering += Render;
+    }
+
+    //private void LateUpdate()
+    //{
+    //    Render();
+    //}
+    //void Render()
+
+    void Render(ScriptableRenderContext ctx, Camera cam)
+    {
+
+        for (int i = 0; i < portals.Length; i++) {
+            portals[i].PrePortalRender ();
+        }
+        for (int i = 0; i < portals.Length; i++) {
+            portals[i].Render (ctx);
+        }
+
+        for (int i = 0; i < portals.Length; i++) {
+            portals[i].PostPortalRender ();
+        }
+
+    }
+
+    private void OnDestroy()
+    {
+        portals = new Portal[0];
+    }
+
+}
