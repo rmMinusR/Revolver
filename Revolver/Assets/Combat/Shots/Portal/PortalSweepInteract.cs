0b23f3c72c5656052c14837f1b5d1f524e48c03e
Robert Christensen <robert.christensen@mymail.champlain.edu>
2022-12-03 18:24:51 -0500

    Portal teleport hint

--- Revolver/Assets/Combat/Shots/Portal/PortalSweepInteract.cs
+++ Revolver/Assets/Combat/Shots/Portal/PortalSweepInteract.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
+[RequireComponent(typeof(Portal))]
 public sealed class PortalSweepInteract : MonoBehaviour
 {
     bool sweeping = false;
@@ -18,19 +19,26 @@ public sealed class PortalSweepInteract : MonoBehaviour
     {
         sweeping = true;
 
-        Vector3 start = transform.position;
-        Vector3 end = start + 2*(args.interactorObject.transform.position-transform.position);
+        Vector3 ownAnchor = transform.position;
+        Vector3 ownDelta = 2*(args.interactorObject.transform.position-ownAnchor);
+
+        Portal portal = GetComponent<Portal>();
+
+        Vector3 otherAnchor = portal.linkedPortal.transform.position;
+        Vector3 otherDelta = portal.linkedPortal.transform.localToWorldMatrix.MultiplyVector( portal.transform.worldToLocalMatrix.MultiplyVector(ownDelta) );
 
         float startTime = Time.time;
         float endTime = Time.time + sweepTime;
         while (Time.time < endTime)
         {
             float t = sweepCurve.Evaluate(Mathf.InverseLerp(startTime, endTime, Time.time));
-            transform.position = Vector3.Lerp(start, end, t);
+            transform.position = ownAnchor + ownDelta * t;
+            portal.linkedPortal.transform.position = otherAnchor + otherDelta * t;
             yield return null;
         }
 
-        transform.position = end;
+        transform.position = ownAnchor + ownDelta;
+        portal.linkedPortal.transform.position = otherAnchor + otherDelta;
         sweeping = false;
     }
 }
